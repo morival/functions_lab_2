8195848964f09d03c01e021232b473fc2a526c12
Katie Sloan <katie.a.sloan@gmail.com>
2020-10-14 17:21:12 +0100

    Initial commit

---
+++ functions_lab_2.py
@@ -0,0 +1,41 @@
+tasks = [
+    { "description": "Wash Dishes", "completed": False, "time_taken": 10 },
+    { "description": "Clean Windows", "completed": False, "time_taken": 15 },
+    { "description": "Make Dinner", "completed": True, "time_taken": 30 },
+    { "description": "Feed Cat", "completed": False, "time_taken": 5 },
+    { "description": "Walk Dog", "completed": True, "time_taken": 60 },
+]
+
+#print a list of uncompleted tasks
+
+# def uncompleted_tasks( list ):
+#   to_do_list = []
+#   for task in list:
+#     if task["completed"] == False:
+#       to_do_list.append(task) 
+#   return to_do_list
+
+# print (uncompleted_tasks(tasks))
+
+#print a list of completed tasks
+# def completed_tasks( list ):
+#   to_do_list = []
+#   for task in list:
+#     if task["completed"] == True:
+#       to_do_list.append(task) 
+#   return to_do_list
+
+# print (completed_tasks(tasks))
+
+#print a list of all task descriptions
+# def task_descriptions( list ):
+#     description_list = []
+#     for task in list:
+#         description_list.append(task["description"])
+#     return description_list
+
+# print (task_descriptions(tasks))
+      
+#print a list of tasks where time taken is at least a given time
+
+ 
